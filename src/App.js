1fb5b9140f3c54554a81cbdec148b0a078477f8a
anand346 <rajanand9039@gmail.com>
2021-08-20 12:42:04 +0530

    update our navigation withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import { Homepage } from './pages/homepage/homepage.component';
+import {Switch,Route,Link} from 'react-router-dom';
 
+const HatsPage = () =>{
+  return (
+    <div>
+      <Link to = "/">Home</Link>
+      Hats Page
+    </div>
+  )
+}
 
 function App() {
   return (
     <div >
-      <Homepage />
+      <Switch>
+        <Route exact path = "/" component = {Homepage}></Route>
+        <Route exact path = "/hats" component = {HatsPage}></Route>
+      </Switch>
     </div>
   );
 }
